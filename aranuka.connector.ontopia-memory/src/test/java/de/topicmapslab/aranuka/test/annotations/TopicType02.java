1573a6d7bfc3e5957612b2a9358e35ae05f0169b
h.niederhausen <h.niederhausen@localhost>
2010-03-01 17:15:51 +0100

    added ontopia memory backend connector including tests

---
+++ aranuka.connector.ontopia-memory/src/test/java/de/topicmapslab/aranuka/test/annotations/TopicType02.java
@@ -0,0 +1,10 @@
+package de.topicmapslab.aranuka.test.annotations;
+
+import de.topicmapslab.aranuka.annotations.Topic;
+
+@Topic(subject_identifier="test:topic_type", name="Topic Type")
+public class TopicType02 {
+
+
+	
+}
