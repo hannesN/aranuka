7b4e30e7aa697dd41dbe1a0f167e4fcd910cd60e
niederhausen <niederhausen@localhost>
2010-10-08 16:50:12 +0200

    bugfix

--- aranuka.connector.majortom/src/test/resources/mkdbprops.sh
+++ aranuka.connector.majortom/src/test/resources/mkdbprops.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-# shell script to create db.properties for hudson
+echo "# shell script to create db.properties for hudson" > db.properties
 
-# database connection properties for the unit tests
+echo "# database connection properties for the unit tests" >> db.properties
 
-#  The host where the database is running or the path to the database for derby or h2.
-echo db_host=$1 > db.properties
+echo "#  The host where the database is running or the path to the database for derby or h2." >> db.properties
+echo db_host=$1 >> db.properties
 
-#name of the database
-db_name=$2 >> db.properties
+echo "#name of the database"  >> db.properties
+echo db_name=$2 >> db.properties
 
-#The port where the database is reachable
+echo "#The port where the database is reachable"  >> db.properties
 echo db_port=$3 >> db.properties
 
-# login for the database
+echo "# login for the database"  >> db.properties
 echo db_login=$4 >> db.properties
 
-#password of the login
+echo "#password of the login"  >> db.properties
 echo db_password=$5 >> db.properties
 
-#database system: h2, mysql, postgresql, derby
+echo "#database system: h2, mysql, postgresql, derby"  >> db.properties
 echo databasesystem=$6 >> db.properties
